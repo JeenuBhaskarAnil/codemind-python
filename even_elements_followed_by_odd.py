43f652e2e3909ab20e8f8eb98ab40a468c74b9f5
JeenuBhaskarAnil <99954362+JeenuBhaskarAnil@users.noreply.github.com>
2022-10-03 10:58:46 +0530

    n=int(input()) l=list(map(int,input().split())) li=[] for i in range(n):     if l[i]%2==0:         li.append(l[i]) for i in range(n):     if l[i]%2!=0:         li.append(l[i]) for i in range(n):     print(li[i],"",end="")

---
+++ even_elements_followed_by_odd.py
@@ -0,0 +1,12 @@
+n=int(input())
+l=list(map(int,input().split()))
+li=[]
+for i in range(n):
+    if l[i]%2==0:
+        li.append(l[i])
+for i in range(n):
+    if l[i]%2!=0:
+        li.append(l[i])
+for i in range(n):
+    print(li[i],"",end="")
+    
