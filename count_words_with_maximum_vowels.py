4174721716f4c026b2aedbb6ae32cf23da3d0f07
JeenuBhaskarAnil <99954362+JeenuBhaskarAnil@users.noreply.github.com>
2022-10-04 19:06:30 +0530

    n=input() l=list(n.split()) s=[] c=0 for i in range(len(l)):     k=0     for j in range(len(l[i])):         if l[i][j]=='a' or l[i][j]=='u' or l[i][j]=='o' or l[i][j]=='i' or l[i][j]=='e':               k=k+1     s.append(k) b=s.count(max(s)) print(b)

---
+++ count_words_with_maximum_vowels.py
@@ -0,0 +1,13 @@
+n=input()
+l=list(n.split())
+s=[]
+c=0
+for i in range(len(l)):
+    k=0
+    for j in range(len(l[i])):
+        if l[i][j]=='a' or l[i][j]=='u' or l[i][j]=='o' or l[i][j]=='i' or l[i][j]=='e':
+              k=k+1
+    s.append(k)
+b=s.count(max(s))
+print(b)
+    
