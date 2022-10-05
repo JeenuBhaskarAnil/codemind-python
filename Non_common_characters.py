085da5da7446836a150d6fd0307e25b6217c7fb7
JeenuBhaskarAnil <99954362+JeenuBhaskarAnil@users.noreply.github.com>
2022-10-05 15:22:52 +0530

    l1=input().lower().split() l2=input().lower().split() l1=''.join(l1) l2=''.join(l2) g=[] for i in l2:     if i not in l1:         g.append(i) for i in l1:     if i not in l2:         g.append(i) c=list(dict.fromkeys(g)) c.sort() print(len(c))

---
+++ Non_common_characters.py
@@ -0,0 +1,14 @@
+l1=input().lower().split()
+l2=input().lower().split()
+l1=''.join(l1)
+l2=''.join(l2)
+g=[]
+for i in l2:
+    if i not in l1:
+        g.append(i)
+for i in l1:
+    if i not in l2:
+        g.append(i)
+c=list(dict.fromkeys(g))
+c.sort()
+print(len(c))
