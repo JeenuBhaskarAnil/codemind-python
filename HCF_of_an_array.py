1608768cfe8dc3797fcb372757ad374663b8da8d
JeenuBhaskarAnil <99954362+JeenuBhaskarAnil@users.noreply.github.com>
2022-10-06 13:42:45 +0530

    n=int(input()) l=list(map(int,input().split())) h=[] cf=[] for i in l:     for j in range(1,i+1):         if i%j==0:             h.append(j) for i in h:     if h.count(i)==n:        cf.append(i) print(max(cf))

---
+++ HCF_of_an_array.py
@@ -0,0 +1,13 @@
+n=int(input())
+l=list(map(int,input().split()))
+h=[]
+cf=[]
+for i in l:
+    for j in range(1,i+1):
+        if i%j==0:
+            h.append(j)
+for i in h:
+    if h.count(i)==n:
+       cf.append(i) 
+print(max(cf))
+    
