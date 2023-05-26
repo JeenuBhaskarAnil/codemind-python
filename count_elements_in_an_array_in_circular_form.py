777f0d23e0df0a0e7e8b745c2f233ee77aa9acfa
JeenuBhaskarAnil <99954362+JeenuBhaskarAnil@users.noreply.github.com>
2023-05-26 11:49:17 +0530

    n=int(input()) l=list(map(int,input().split())) k=[] c=0 k.append(l[-1]) for i in range(n):     k.append(l[i]) k.append(l[0]) for i in range(1,n+1):     if ((k[i-1] % 2 == 0 and k[i+1] % 2 != 0) or (k[i-1] % 2 != 0 and k[i+1] % 2 == 0)):         c=c+1 print(c)

--- count_elements_in_an_array_in_circular_form.py
+++ count_elements_in_an_array_in_circular_form.py
@@ -1,18 +1,12 @@
 n=int(input())
-li=list(map(int,input().split()))
-l=[]
+l=list(map(int,input().split()))
+k=[]
 c=0
-l.append(li[len(li)-1])
+k.append(l[-1])
 for i in range(n):
-    l.append(li[i])
-l.append(li[0])
+    k.append(l[i])
+k.append(l[0])
 for i in range(1,n+1):
-    if (l[i-1]%2!=0 and l[i+1]%2==0) or (l[i-1]%2==0 and l[i+1]%2!=0):
+    if ((k[i-1] % 2 == 0 and k[i+1] % 2 != 0) or (k[i-1] % 2 != 0 and k[i+1] % 2 == 0)):
         c=c+1
 print(c)
-
-
-
-
-
-
