4e774496a147dcf92b985669cc91d1c102a66a1f
JeenuBhaskarAnil <99954362+JeenuBhaskarAnil@users.noreply.github.com>
2023-05-27 11:35:11 +0530

    li=input().lower().split() l=li[1:len(li)] r="" for i in li[0]:     s=0     for j in l:         if i in j:             s+=1             j=j[0:j.index(i)]+j[j.index(i)+1:len(j)]     if len(l)==s:         r+=i print(len(r))

---
+++ Common_characters_-_2.py
@@ -0,0 +1,12 @@
+li=input().lower().split()
+l=li[1:len(li)]
+r=""
+for i in li[0]:
+    s=0
+    for j in l:
+        if i in j:
+            s+=1
+            j=j[0:j.index(i)]+j[j.index(i)+1:len(j)]
+    if len(l)==s:
+        r+=i
+print(len(r))
