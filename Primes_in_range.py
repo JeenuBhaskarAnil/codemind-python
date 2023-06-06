c6a9c4bb43ee2a39b2a97022783af5c8178ad481
JeenuBhaskarAnil <99954362+JeenuBhaskarAnil@users.noreply.github.com>
2023-06-06 15:12:13 +0530

    from math import sqrt def p(n):     for i in range(2,int(sqrt(n))+1):         if n%i==0:             return False     return True c=0 for i in range(int(input()),int(input())+1):     if p(i) and i!=1:         c+=1 print(c)

--- Primes_in_range.py
+++ Primes_in_range.py
@@ -1,16 +1,11 @@
-import math
+from math import sqrt
 def p(n):
-    if n==1:
-        return False
-    s=int(math.sqrt(n))
-    for i in range(2,s+1):
+    for i in range(2,int(sqrt(n))+1):
         if n%i==0:
             return False
     return True
-a=int(input())
-b=int(input())
 c=0
-for i in range(a,b+1):
-    if p(i):
+for i in range(int(input()),int(input())+1):
+    if p(i) and i!=1:
         c+=1
 print(c)
