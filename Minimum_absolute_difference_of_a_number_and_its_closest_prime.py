9ef46a714bf7190ae30401e485ef1cbc96c89af8
JeenuBhaskarAnil <99954362+JeenuBhaskarAnil@users.noreply.github.com>
2023-06-09 10:38:27 +0530

    def f(n):     for i in range(2,n):         if n%i==0:             return 0>1     return 1>0
    n=int(input())
    b=[]
    for i in range(2,n):
        if f(i):
            b.append(i)
    for i in range(n+1,n**3):
        if f(i):
            b.append(i)
            break
    l=b[-2]
    r=b[-1]
    ld=abs(l-n)
    rd=abs(r-n)
    if f(n):
        print("0")
    elif ld>=rd:
        print(rd)
    else:
        print(ld)

--- Minimum_absolute_difference_of_a_number_and_its_closest_prime.py
+++ Minimum_absolute_difference_of_a_number_and_its_closest_prime.py
@@ -1,23 +1,25 @@
+def f(n):
+    for i in range(2,n):
+        if n%i==0:
+            return 0>1
+    return 1>0
+   
 n=int(input())
-for i in range(n,1,-1):
-    c=0
-    for j in range(2,i):
-        if i%j==0:
-            c=c+1
-    if c==0:
-        f=i
+b=[]
+for i in range(2,n):
+    if f(i):
+        b.append(i)
+for i in range(n+1,n**3):
+    if f(i):
+        b.append(i)
         break
-for i in range(n+1,n*n):
-    c=0
-    for j in range(2,i):
-        if i%j==0:
-            c=c+1
-    if c==0:
-        g=i
-        break
-if n-f>g-n:
-    print(g-n)
+l=b[-2]
+r=b[-1]
+ld=abs(l-n)
+rd=abs(r-n)
+if f(n):
+    print("0")
+elif ld>=rd:
+    print(rd)
 else:
-    print(n-f)
-    
-    
+    print(ld)
