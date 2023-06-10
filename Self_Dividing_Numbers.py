dcb3b85b92edf635b989e62ccd11162223ffc9da
JeenuBhaskarAnil <99954362+JeenuBhaskarAnil@users.noreply.github.com>
2023-06-10 11:26:47 +0530

    def f(n):     m=n     while n:         r=n%10         if r==0:             return False         else:             if m%r!=0:                 return False         n=n//10     return True for i in range(int(input()),int(input())+1):     if f(i):         print(i,end=" ")

--- Self_Dividing_Numbers.py
+++ Self_Dividing_Numbers.py
@@ -1,21 +1,14 @@
-n=int(input())
-m=int(input())
-for i in range(n,m+1):
-    h=i
-    u=0
-    if i%10!=0:
-        s=0
-        while i:
-            k=i%10
-            i=i//10
-            if h%k!=0:
-                s=s+1
-        if s==0:
-            print(h,"",end="")
-            
-
-
-
-
-
-        
+def f(n):
+    m=n
+    while n:
+        r=n%10
+        if r==0:
+            return False
+        else:
+            if m%r!=0:
+                return False
+        n=n//10
+    return True
+for i in range(int(input()),int(input())+1):
+    if f(i):
+        print(i,end=" ")
